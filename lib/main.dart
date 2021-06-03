935e62162f02df0ee188c59aeaf78c1a20613149
JameeShahriyarAmit <81066357+JameeShahriyarAmit@users.noreply.github.com>
2021-06-04 03:09:54 +0600

    Release 1: All screens and navigation between the screens
    Release 1 is for Deliverable 1 and contains all the screens needed by the application. It also uses named routes for navigation through the screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:money_manager/router.dart';
-import 'package:money_manager/screens/Add_Custom_Category.dart';
-import 'package:money_manager/screens/Add_Expense.dart';
-import 'package:money_manager/screens/budget.dart';
-import 'package:money_manager/screens/guest_user_dash.dart';
-import 'package:money_manager/screens/mainScreen.dart';
-import 'package:money_manager/screens/premiumUserLogin.dart';
-import 'package:money_manager/screens/premiumUserRegistration.dart';
-import 'package:money_manager/screens/registered_user_dash.dart';
-import 'package:money_manager/screens/update_budget.dart';
-import 'package:money_manager/screens/view_expense.dart';
 
 import 'constants.dart';
 
@@ -17,17 +7,7 @@ void main() => runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "flutter_navigation_sample",
     theme: ThemeData(primarySwatch: Colors.indigo),
-    //  home: GuestUserDash()
-    //  home: RegisteredUserDash()
-     //home: MainScreen()
-    // home: PremiumUserLogin()
-    // home: PremiumUserRegistration()
-    //  home: CustomExpenseCategory()
-        // home : AddExpense()
-        // home: BudgetPage(),
-        // home: UpdateBudget(),
-        // home: ViewExpense(),
-
+    
      initialRoute: homeRoute,
      onGenerateRoute: createRoute,
     ));
