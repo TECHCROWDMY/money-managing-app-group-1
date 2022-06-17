35f65b080c109c5e66564b1794569612c43216d4
thilaks <thilak7c@gmail.com>
2022-06-17 19:50:35 +0800

    This is my first commit!

--- lib/screens/Add_Custom_Category.dart
+++ lib/screens/Add_Custom_Category.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:money_manager/Widgets/registered_menu_widget.dart';
 
-
-
 class CustomExpenseCategory extends StatefulWidget {
   @override
   CustomExpenseCategoryState createState() => CustomExpenseCategoryState();
 }
 
 class CustomExpenseCategoryState extends State<CustomExpenseCategory> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,37 +21,35 @@ class CustomExpenseCategoryState extends State<CustomExpenseCategory> {
         ),
         centerTitle: true,
       ),
-      drawer: RegisteredMenu(), 
-      body:
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                  margin: EdgeInsets.fromLTRB(70.0, 35.0, 5.0, 40.0) ,
-                  width: 250,
-                  child: TextField(
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Enter Custom Category',
-                      hintText: 'Food, laundry,other expenses...',
-                    ),
-                    autofocus: false,
-                  )),
-
-              Container(
-                margin: EdgeInsets.fromLTRB(60.0, 35.0, 5.0, 20.0) ,
-                width: 150,
-                child: FloatingActionButton.extended(
-                  onPressed: (){},
-                  label: Text('Add Category'),
-                  backgroundColor: Colors.indigo,
+      drawer: RegisteredMenu(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+              margin: EdgeInsets.fromLTRB(70.0, 35.0, 5.0, 40.0),
+              width: 250,
+              child: TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter Custom Category',
+                  hintText: 'Food, laundry,other expenses...',
                 ),
-              ),
-            ],
+                autofocus: false,
+              )),
+          Container(
+            margin: EdgeInsets.fromLTRB(60.0, 35.0, 5.0, 20.0),
+            width: 150,
+            child: FloatingActionButton.extended(
+              onPressed: () {},
+              label: Text('Add Category'),
+              backgroundColor: Colors.indigo,
+            ),
           ),
-      );
+        ],
+      ),
+    );
   }
 }
 
